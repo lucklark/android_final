7640ab8bb8088ea63ab8bc55e4cc9e67cc5a981d
13402977489 <517707320@qq.com>
2020-06-11 21:31:40 +0800

    大作业初版

---
+++ notes/app/src/main/java/com/homework/notes/SpacedService.java
@@ -0,0 +1,92 @@
+package com.homework.notes;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.*;
+import androidx.core.app.NotificationCompat;
+
+import java.util.List;
+
+
+
+public class SpacedService extends Service {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                NotesDataSource nds = new NotesDataSource(SpacedService.this);
+                List<NoteItems> items = nds.getAllNotesForNotification();
+                for(NoteItems item : items)
+                {
+                    SpacedService.this.notification(item.title,item.content);
+                    nds.incrementTotalReviews(item.content);
+                }
+                handler.postDelayed(this, 120000/2); //now is every 2 minutes
+            }
+        }, 120000/2);
+
+
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void notification(String title, String message)
+    {
+        int random = 1 + (int)(Math.random() * ((100 - 1) + 1)); // generates random number from 1 to 100
+        int num = message.hashCode();
+
+        String id = "channel";
+        String name = "记录 " + title;
+        NotificationManager notificationManager = (NotificationManager)
+                this.getSystemService(NOTIFICATION_SERVICE);
+
+        Intent intent = new Intent(this,AnswerCard.class);
+        intent.putExtra("title",title);
+        intent.putExtra("content",message);
+        intent.putExtra("from","notification");
+        PendingIntent i=PendingIntent.getActivity(this, random,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        Notification notification = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {//判断API
+            NotificationChannel mChannel = new NotificationChannel(id, name, NotificationManager.IMPORTANCE_LOW);
+            notificationManager.createNotificationChannel(mChannel);
+            notification = new Notification.Builder(this)
+                    .setChannelId(id)
+                    .setContentTitle("记录")
+                    .setContentText("您有一项记录 " + title)
+                    .setContentIntent(i)
+                    .setWhen(System.currentTimeMillis())
+                    .setSmallIcon(R.mipmap.ic_launcher_round).build();
+        }else{
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                    .setContentTitle("记录")
+                    .setContentText("您有一项记录")
+                    .setSmallIcon(R.mipmap.ic_launcher_round)
+                    .setContentIntent(i)
+                    .setWhen(System.currentTimeMillis())
+                    .setOngoing(true)
+                    .setChannelId(id);//无效
+            notification = notificationBuilder.build();
+        }
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+        notificationManager.notify(num,notification);
+
+
+    }
+}
